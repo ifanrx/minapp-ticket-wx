76b3175b191e3688739974aa0ac1c55ad9c9131c
guochaohong <guochaohong@ifanr.com>
2019-03-29 17:46:15 +0800

    增加 SDK 版本验证

--- src/index.js
+++ src/index.js
@@ -8,6 +8,11 @@ Component({
   data: {
     hideButton: false,
   },
+  ready() {
+    if (typeof wx.BaaS === 'undefined' || /^v?(\d+)\./i.exec(wx.BaaS._config.VERSION)[1] < 2) {
+      throw new Error('请使用 v2.0 版本以上的知晓云 SDK')
+    }
+  },
   methods: {
     reportTicket: e => {
       wx.BaaS.wxReportTicket(e.detail.formId)
