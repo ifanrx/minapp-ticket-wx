5939a1f829abe5c40aec0e6fa83d45f4c40f5fd6
guochaohong <guochaohong@ifanr.com>
2019-03-29 15:12:44 +0800

    update gulp settings info

--- gulpfile.js
+++ gulpfile.js
@@ -1,9 +1,9 @@
 const gulp = require('gulp')
 gulp.task('copy', () => {
-  return gulp.src('../src/**/*')
-    .pipe(gulp.dest('./components/minapp-ticket-tx'))
+  return gulp.src('./src/**/*')
+    .pipe(gulp.dest('./demo/components/minapp-ticket-wx'))
 })
 gulp.task('watch', () => {
-  return gulp.watch('../src/**/*', gulp.series('copy'))
+  return gulp.watch('./src/**/*', gulp.series('copy'))
 })
 gulp.task('default', gulp.series('copy', 'watch'))
